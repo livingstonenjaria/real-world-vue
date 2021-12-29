42c3be9da1a41190ff32d22a0d83fdfc4a1fdfbf
Livingstone <livingstonenjaria4@gmail.com>
2021-12-29 14:24:36 +0300

    Modified the eslint file to handle prettier endoffile errors, deleted default vue view files, created new view files and modified vue router to include new view files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,22 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', 'plugin:prettier/recommended', '@vue/prettier'],
+  extends: [
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    '@vue/prettier',
+  ],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 }
---
+++ src/views/EventShow.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Events Show</h1>
+  </div>
+</template>
