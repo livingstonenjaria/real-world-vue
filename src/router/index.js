143f456862c0bfde94197c898d28cda2123ec785
Livingstone <livingstonenjaria4@gmail.com>
2022-01-04 00:43:39 +0300

    Fleshed out EventCreate and implemented first mutation and action

--- src/router/index.js
+++ src/router/index.js
@@ -13,16 +13,17 @@ const routes = [
     component: EventList,
   },
   {
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate,
+  },
+  {
     path: '/event/:id',
     name: 'event-show',
     component: EventShow,
     props: true,
   },
-  {
-    path: '/event/create',
-    name: 'event-create',
-    component: EventCreate,
-  },
+  ,
 ]
 
 const router = new VueRouter({
