0719a530d84fed43113c770f57b47e7da8e1897d
Livingstone <livingstonenjaria4@gmail.com>
2022-01-04 17:14:48 +0300

    Added 404 error pages

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import EventList from '../views/EventList.vue'
 import EventShow from '../views/EventShow.vue'
 import store from '../store'
 import NProgress from 'nprogress'
+import NotFound from '../views/NotFound.vue'
+import NetworkIssue from '../views/NetworkIssue.vue'
 
 Vue.use(VueRouter)
 
@@ -26,13 +28,36 @@ const routes = [
     component: EventShow,
     props: true,
     beforeEnter(routeTo, routeFrom, next) {
-      store.dispatch('event/fetchEvent', routeTo.params.id).then((event) => {
-        routeTo.params.event = event
-        next()
-      })
+      store
+        .dispatch('event/fetchEvent', routeTo.params.id)
+        .then((event) => {
+          routeTo.params.event = event
+          next()
+        })
+        .catch((error) => {
+          if (error.response && error.response.status == 404) {
+            next({ name: '404', params: { resource: 'event' } })
+          } else {
+            next({ name: 'network-issue' })
+          }
+        })
     },
   },
-  ,
+  {
+    path: '/404',
+    name: '404',
+    component: NotFound,
+    props: true,
+  },
+  {
+    path: '/network-issue',
+    name: 'network-issue',
+    component: NetworkIssue,
+  },
+  {
+    path: '*',
+    redirect: { name: '404', params: { resource: 'page' } },
+  },
 ]
 
 const router = new VueRouter({
